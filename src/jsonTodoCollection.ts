35a8075e895b137234f74935f442693c16b02a19
Furkan Alp <furkanalptokac@gmail.com>
2021-05-11 12:07:52 +0300

    format code

--- src/jsonTodoCollection.ts
+++ src/jsonTodoCollection.ts
@@ -5,46 +5,51 @@ import { TodoItem } from './todoItem';
 import { TodoCollection } from './todoCollection';
 
 type schemaType = {
-    tasks: {
-        id: number,
-        task: string,
-        complete: boolean
-    }[]
+  tasks: {
+    id: number;
+    task: string;
+    complete: boolean;
+  }[];
 };
 
 export class JsonTodoCollection extends TodoCollection {
-    private database: lowdb.LowdbSync<schemaType>;
+  private database: lowdb.LowdbSync<schemaType>;
 
-    constructor (public userName: string, todoItems: TodoItem[] = []) {
-        super(userName, []);
-        this.database = lowdb(new FileSync('Todos.json'));
+  constructor(public userName: string, todoItems: TodoItem[] = []) {
+    super(userName, []);
+    this.database = lowdb(new FileSync('Todos.json'));
 
-        if (this.database.has('tasks').value()) {
-            const dbItems = this.database.get('tasks').value();
-            dbItems.forEach(item => this.itemMap.set(item.id, new TodoItem(item.id, item.task, item.complete)));
-        } else {
-            this.database.set('tasks', todoItems).write();
-            todoItems.forEach(item => this.itemMap.set(item.id, item));
-        }
+    if (this.database.has('tasks').value()) {
+      const dbItems = this.database.get('tasks').value();
+      dbItems.forEach((item) =>
+        this.itemMap.set(
+          item.id,
+          new TodoItem(item.id, item.task, item.complete)
+        )
+      );
+    } else {
+      this.database.set('tasks', todoItems).write();
+      todoItems.forEach((item) => this.itemMap.set(item.id, item));
     }
+  }
 
-    addTodo (task: string) : number {
-        const result = super.addTodo(task);
-        this.storeTasks();
-        return result;
-    }
+  addTodo(task: string): number {
+    const result = super.addTodo(task);
+    this.storeTasks();
+    return result;
+  }
 
-    markComplete (id: number, complete: boolean) : void {
-        super.markComplete(id, complete);
-        this.storeTasks();
-    }
+  markComplete(id: number, complete: boolean): void {
+    super.markComplete(id, complete);
+    this.storeTasks();
+  }
 
-    removeComplete () : void {
-        super.removeComplete();
-        this.storeTasks();
-    }
+  removeComplete(): void {
+    super.removeComplete();
+    this.storeTasks();
+  }
 
-    private storeTasks () {
-        this.database.set('tasks', [...this.itemMap.values()]).write();
-    }
+  private storeTasks() {
+    this.database.set('tasks', [...this.itemMap.values()]).write();
+  }
 }
